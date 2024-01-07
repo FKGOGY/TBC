15caa02ade18697259b8d69681d8c7b251a3b156
FKGOGY <123078766+FKGOGY@users.noreply.github.com>
2024-01-07 14:16:03 +0100

    Add files via upload

--- Game/CommandProcessor.gd
+++ Game/CommandProcessor.gd
@@ -1,9 +1,36 @@
 extends Node
 
-
-func _ready():
+func initialize(starting_room):
 	pass
 
+var commandError := "Unknown Command"
 
-func _process(delta):
-	pass
+func process_command(input: String) -> String:
+	input.to_lower()
+	var words = input.split(" ", false)
+	if words.size() == 0:
+		return "No words were parsed."
+	
+	var firstWord = words[0]
+	var secondWord = ""
+	
+	if words.size() > 1:
+		secondWord = words[1]
+	
+	match firstWord:
+		"go":
+			return go(secondWord)
+		"help":
+			return help()
+		_:
+			return commandError
+
+
+func go(secondWord: String) -> String:
+	if secondWord == "":
+		return "Go where?"
+	
+	return "You go to " + secondWord
+
+func help() -> String:
+	return "go [location] \nopen [what]"
--- Game/Game.gd
+++ Game/Game.gd
@@ -1,31 +1,45 @@
 extends Control
 
+const Response = preload("res://Game/Response.tscn")
 const InputResponse = preload("res://Game/InputResponse.tscn")
 
 @onready var historyRows = $Background/MarginContainer/Rows/GameInfo/Scroll/HistoryRows
 @onready var scroll = $Background/MarginContainer/Rows/GameInfo/Scroll
 @onready var scrollBar = scroll.get_v_scroll_bar()
+@onready var command_processor = $CommandProcessor
 
 var max_scroll_length := 0
 @export var max_line_remembered: int = 15
 
+
 func _ready():
 	scrollBar.changed.connect(_on_scroll_size_changed)
 	max_scroll_length = scrollBar.max_value
+	var starting_message = Response.instantiate()
+	starting_message.text = "Start Message" 
+	add_response_to_game(starting_message)
+
 
 func _on_scroll_size_changed():
 	if max_scroll_length != scrollBar.max_value:
 		max_scroll_length = scrollBar.max_value
 		scrollBar.value = max_scroll_length
 
+
 func _on_input_text_submitted(new_text: String) -> void:
 	if new_text.is_empty():
 		return
 	
 	var input_response := InputResponse.instantiate()
-	input_response.set_text(new_text,"Odpověď na příkaz")
-	historyRows.add_child(input_response)
+	var response = command_processor.process_command(new_text)
+	input_response.set_text(new_text, response)
+	add_response_to_game(input_response)
+
+
+func add_response_to_game(response: Control):
+	historyRows.add_child(response)
 	delete_history_to_limit()
+
 
 func delete_history_to_limit():
 	if historyRows.get_child_count() > max_line_remembered:
